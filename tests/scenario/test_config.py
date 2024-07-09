41d076bb597d1cbaf451d8afd37a4ae7a021d6f5
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2024-07-09 11:11:57 +0200

    Add containers to new config test

--- tests/scenario/test_config.py
+++ tests/scenario/test_config.py
@@ -4,7 +4,9 @@ from charm import TempoCoordinatorCharm
 from tempo_cluster import TempoClusterRequirerUnitData
 
 
-def test_memberlist_multiple_members(context, all_worker, s3):
+def test_memberlist_multiple_members(
+    context, all_worker, s3, nginx_container, nginx_prometheus_exporter_container
+):
     workers_no = 3
     all_worker = all_worker.replace(
         remote_units_data={
@@ -23,7 +25,11 @@ def test_memberlist_multiple_members(context, all_worker, s3):
             for worker_idx in range(workers_no)
         },
     )
-    state = State(leader=True, relations=[all_worker, s3])
+    state = State(
+        leader=True,
+        relations=[all_worker, s3],
+        containers=[nginx_container, nginx_prometheus_exporter_container],
+    )
     with context.manager(all_worker.changed_event, state) as mgr:
         charm: TempoCoordinatorCharm = mgr.charm
         assert charm.tempo_cluster.gather_addresses() == set(
