e7f86df3acf3d28c9751b66c62cdf1111e617528
Pietro Pasotti <starfire.daemon@gmail.com>
2024-09-11 14:59:51 +0200

    added worker charm config

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -98,10 +98,10 @@ def get_relation_by_endpoint(relations, local_endpoint, remote_endpoint, remote_
         r
         for r in relations
         if (
-            (r["endpoint"] == local_endpoint and r["related-endpoint"] == remote_endpoint)
-            or (r["endpoint"] == remote_endpoint and r["related-endpoint"] == local_endpoint)
-        )
-        and remote_obj in r["related-units"]
+                   (r["endpoint"] == local_endpoint and r["related-endpoint"] == remote_endpoint)
+                   or (r["endpoint"] == remote_endpoint and r["related-endpoint"] == local_endpoint)
+           )
+           and remote_obj in r["related-units"]
     ]
     if not matches:
         raise ValueError(
@@ -128,7 +128,7 @@ class UnitRelationData:
 
 
 def get_content(
-    obj: str, other_obj, include_default_juju_keys: bool = False, model: str = None
+        obj: str, other_obj, include_default_juju_keys: bool = False, model: str = None
 ) -> UnitRelationData:
     """Get the content of the databag of `obj`, as seen from `other_obj`."""
     unit_name, endpoint = obj.split(":")
@@ -170,11 +170,11 @@ class RelationData:
 
 
 def get_relation_data(
-    *,
-    provider_endpoint: str,
-    requirer_endpoint: str,
-    include_default_juju_keys: bool = False,
-    model: str = None,
+        *,
+        provider_endpoint: str,
+        requirer_endpoint: str,
+        include_default_juju_keys: bool = False,
+        model: str = None,
 ):
     """Get relation databags for a juju relation.
 
@@ -216,12 +216,12 @@ async def run_command(model_name: str, app_name: str, unit_num: int, command: li
 
 
 def present_facade(
-    interface: str,
-    app_data: Dict = None,
-    unit_data: Dict = None,
-    role: Literal["provide", "require"] = "provide",
-    model: str = None,
-    app: str = "facade",
+        interface: str,
+        app_data: Dict = None,
+        unit_data: Dict = None,
+        role: Literal["provide", "require"] = "provide",
+        model: str = None,
+        app: str = "facade",
 ):
     """Set up the facade charm to present this data over the interface ``interface``."""
     data = {
@@ -289,9 +289,21 @@ async def deploy_and_configure_minio(ops_test: OpsTest):
     assert action_result.status == "completed"
 
 
+def tempo_worker_charm_and_channel():
+    """Tempo worker charm used for integration testing.
+
+    Build once per session and reuse it in all integration tests to save some minutes/hours.
+    You can also set `TEMPO_WORKER_CHARM` env variable to use an already existing built charm.
+    """
+    if path_from_env := os.getenv("TEMPO_WORKER_CHARM"):
+        return Path(path_from_env).absolute(), None
+    return "tempo-worker-k8s", "edge"
+
+
 async def deploy_cluster(ops_test: OpsTest, tempo_app=APP_NAME):
+    tempo_worker_charm_url, channel = tempo_worker_charm_and_channel()
     await ops_test.model.deploy(
-        "tempo-worker-k8s", application_name=WORKER_NAME, channel="edge", trust=True
+        tempo_worker_charm_url, application_name=WORKER_NAME, channel=channel, trust=True
     )
     await ops_test.model.deploy(S3_INTEGRATOR, channel="edge")
 
@@ -327,7 +339,7 @@ async def get_traces_patiently(tempo_host, service_name="tracegen-otlp_http", tl
 
 
 async def emit_trace(
-    endpoint, ops_test: OpsTest, nonce, proto: str = "otlp_http", verbose=0, use_cert=False
+        endpoint, ops_test: OpsTest, nonce, proto: str = "otlp_http", verbose=0, use_cert=False
 ):
     """Use juju ssh to run tracegen from the tempo charm; to avoid any DNS issues."""
     cmd = (
