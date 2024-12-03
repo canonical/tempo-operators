3156809a44c7e909d5947a8076c53ed3d20b2cb7
PietroPasotti <starfire.daemon@gmail.com>
2024-12-03 08:32:47 +0100

    unpin CRI branch (#83)
    * interface tests passing
    
    * fmt
    
    * fmt
    
    * removed dupe patch
    
    * cleaned up patches in conftes
    
    * test against CRI@main

--- tests/interface/test_grafana_source.py
+++ tests/interface/test_grafana_source.py
@@ -6,7 +6,6 @@ from interface_tester import InterfaceTester
 def test_grafana_datasource_v0_interface(grafana_datasource_tester: InterfaceTester):
     grafana_datasource_tester.configure(
         interface_name="grafana_datasource",
-        branch="grafana-datasource-interface",
         interface_version=0,
     )
     grafana_datasource_tester.run()
