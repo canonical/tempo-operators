db5f71c97c7ff193c7780cd7218ae72ba08b56fe
Pietro Pasotti <pietro.pasotti@canonical.com>
2025-06-24 10:27:42 +0200

    fixed itest script

--- itest.sh
+++ itest.sh
@@ -12,17 +12,19 @@ if [[ $# -eq 0 ]] ; then
     exit 0
 fi
 
-if [[ $1 -eq "coordinator" ]] ; then
+if [[ $1 == "coordinator" ]] ; then
+  echo $1
   echo "RUNNING (from ./coordinator):" tox -e integration -- "${@:2}"
   pushd ./coordinator
   tox -e integration -- "${@:2}"
   popd
-elif [[ $1 -eq "worker" ]] ; then
+elif [[ $1 == "worker" ]] ; then
   pushd ./worker
   echo "RUNNING (from ./worker):" tox -e integration -- "${@:2}"
   tox -e integration -- "${@:2}"
   popd
 else
-  echo "RUNNING (from ./):" tox -e integration -- "${@:2}"
-  tox -e integration -- "${@:2}"
+  # $1 could very well be "-k", so we need to pass it forward
+  echo "RUNNING (from ./):" tox -e integration -- "${@:1}"
+  tox -e integration -- "${@:1}"
 fi
