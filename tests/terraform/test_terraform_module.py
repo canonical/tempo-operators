25e8aebd975b6b7bcef2ab1d4133c6e78d7f22ee
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2025-10-01 13:16:57 +0200

    chore: Move terraform module to this repo; add tests (#192)

---
+++ tests/terraform/test_terraform_module.py
@@ -0,0 +1,73 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+import logging
+import pathlib
+import subprocess
+import jubilant
+import pytest
+import requests
+from time import sleep
+
+from tenacity import retry, stop_after_attempt, wait_fixed
+
+from pytest_bdd import given, when, then
+
+logger = logging.getLogger(__name__)
+
+TESTS_DIR = pathlib.Path(__file__).parent.resolve()
+
+
+def get_unit_ip_address(juju: jubilant.Juju, app_name: str, unit_no: int):
+    """Return a juju unit's IP address."""
+    return juju.status().apps[app_name].units[f"{app_name}/{unit_no}"].address
+
+
+@pytest.fixture(scope="module")
+def juju():
+    with jubilant.temp_model() as tm:
+        yield tm
+
+
+@pytest.fixture(scope="module")
+def seaweedfs(juju):
+    juju.deploy("seaweedfs-k8s", "seaweedfs", channel="latest/edge")
+    juju.wait(
+        lambda status: jubilant.all_active(status, "seaweedfs"),
+        timeout=15 * 10,
+        delay=5,
+        successes=3,
+    )
+
+
+@pytest.fixture(scope="module")
+@retry(stop=stop_after_attempt(20), wait=wait_fixed(2))
+def bucket(juju, seaweedfs):
+    endpoint = f"http://{get_unit_ip_address(juju, 'seaweedfs', 0)}:8333"
+    requests.put(f"{endpoint}/tempo/")
+
+
+@given("a juju model")
+@when("you run terraform apply using the provided module")
+def test_terraform_apply(juju, bucket):
+    endpoint = f"http://{get_unit_ip_address(juju, 'seaweedfs', 0)}:8333"
+    subprocess.check_call(["terraform", f"-chdir={TESTS_DIR}", "init"])
+    out = subprocess.check_output(
+        f'terraform -chdir={TESTS_DIR} apply -var "model={juju.model}" -var "endpoint={endpoint}" -auto-approve',
+        shell=True,
+    )
+
+
+@then("tempo charms are deployed and active")
+def test_active(juju):
+    juju.wait(
+        lambda status: jubilant.all_active(
+            status,
+            "tempo",
+            "tempo-compactor",
+            "tempo-query-frontend",
+            "tempo-distributor",
+            "tempo-ingester",
+            "tempo-querier",
+        ),
+        timeout=60 * 10,
+    )
