9c1968658ccb85ed1683ebb8800cc98bac78d797
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2025-10-23 11:23:11 +0200

    chore: Unpin s3-integrator revision

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -197,7 +197,7 @@ variable "worker_revision" {
 variable "s3_integrator_revision" {
   description = "Revision number of the s3-integrator application"
   type        = number
-  default     = 157 # FIXME: https://github.com/canonical/observability/issues/342
+  default     = null
 }
 
 # -------------- # Storage directives --------------
