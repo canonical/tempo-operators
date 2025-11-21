333663d6a4fa0f3ea65b39c02c45c3b9003e4f2d
Simon Aronsson <simme@arcticbit.se>
2025-11-21 14:53:04 +0100

    Modify outputs to handle null values for modules
    Updated output values to handle empty module cases.
    
    Signed-off-by: Simon Aronsson <simme@arcticbit.se>

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,12 +3,13 @@ output "app_names" {
     {
       tempo_s3_integrator     = juju_application.s3_integrator.name,
       tempo_coordinator       = module.tempo_coordinator.app_name,
-      tempo_querier           = module.tempo_querier.app_name,
-      tempo_query_frontend    = module.tempo_query_frontend.app_name,
-      tempo_ingester          = module.tempo_ingester.app_name,
-      tempo_distributor       = module.tempo_distributor.app_name,
-      tempo_compactor         = module.tempo_compactor.app_name,
-      tempo_metrics_generator = module.tempo_metrics_generator.app_name,
+      tempo_querier           = length(module.tempo_querier) == 0 ? null : module.tempo_querier[0].app_name
+      tempo_query_frontend    = length(module.tempo_query_frontend) == 0 ? null : module.tempo_query_frontend[0].app_name
+      tempo_ingester          = length(module.tempo_ingester) == 0 ? null : module.tempo_ingester[0].app_name
+      tempo_distributor       = length(module.tempo_distributor) == 0 ? null : module.tempo_distributor[0].app_name
+      tempo_compactor         = length(module.tempo_compactor) == 0 ? null : module.tempo_compactor[0].app_name
+      tempo_metrics_generator = length(module.tempo_metrics_generator) == 0 ? null : module.tempo_metrics_generator.app_name
+      tempo_all               = length(module.tempo_all) == 0 ? null : module.tempo_all[0].app_name
     }
   )
   description = "All application names which make up this product module"
