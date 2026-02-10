550851631951000c3c9df93b6e364caaf21268d0
Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>
2026-02-10 13:10:21 +0100

    Unifies the storage directives variable name
    Signed-off-by: Bartlomiej Gmerek <bartlomiej.gmerek@canonical.com>

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -214,37 +214,37 @@ variable "coordinator_storage_directives" {
   default     = {}
 }
 
-variable "compactor_storage_directives" {
+variable "compactor_worker_storage_directives" {
   description = "Map of storage used by the compactor worker application, which defaults to 1 GB, allocated by Juju"
   type        = map(string)
   default     = {}
 }
 
-variable "distributor_storage_directives" {
+variable "distributor_worker_storage_directives" {
   description = "Map of storage used by the distributor worker application, which defaults to 1 GB, allocated by Juju"
   type        = map(string)
   default     = {}
 }
 
-variable "ingester_storage_directives" {
+variable "ingester_worker_storage_directives" {
   description = "Map of storage used by the ingester worker application, which defaults to 1 GB, allocated by Juju"
   type        = map(string)
   default     = {}
 }
 
-variable "metrics_generator_storage_directives" {
+variable "metrics_generator_worker_storage_directives" {
   description = "Map of storage used by the metrics-generator worker application, which defaults to 1 GB, allocated by Juju"
   type        = map(string)
   default     = {}
 }
 
-variable "querier_storage_directives" {
+variable "querier_worker_storage_directives" {
   description = "Map of storage used by the querier worker application, which defaults to 1 GB, allocated by Juju"
   type        = map(string)
   default     = {}
 }
 
-variable "query_frontend_storage_directives" {
+variable "query_frontend_worker_storage_directives" {
   description = "Map of storage used by the query-frontend worker application, which defaults to 1 GB, allocated by Juju"
   type        = map(string)
   default     = {}
