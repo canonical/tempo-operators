8f867814af80c2614290debf25e5e5350b3414d7
Michael Thamm <mike.thamm@canonical.com>
2026-05-19 15:08:22 +0200

    chore: TF

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -20,7 +20,7 @@ output "provides" {
     grafana_dashboard = "grafana-dashboard",
     grafana_source    = "grafana-source",
     metrics_endpoint  = "metrics-endpoint",
-    provide_cmr_mesh  = "provide-cmr-mesh",
+    provide_cmr_mesh  = module.mimir_coordinator.provides.provide_cmr_mesh,
     tracing           = "tracing",
   }
   description = "All Juju integration endpoints where the charm is the provider"
@@ -34,8 +34,8 @@ output "requires" {
     send-remote-write  = "send-remote-write",
     receive_datasource = "receive-datasource"
     catalogue          = "catalogue",
-    require_cmr_mesh   = "require-cmr-mesh",
-    service_mesh       = "service-mesh",
+    require_cmr_mesh   = module.mimir_coordinator.provides.require_cmr_mesh,
+    service_mesh       = module.mimir_coordinator.provides.service_mesh,
   }
   description = "All Juju integration endpoints where the charm is the requirer"
 }
